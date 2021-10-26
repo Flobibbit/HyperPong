fb8e9f56084ee3c240b383c6e8f036763861b659
Phiedie <p.sautmann@sozialbank.de>
2021-10-26 18:43:28 +0200

    console logs entfernt, comments

--- src/pages/ball.js
+++ src/pages/ball.js
@@ -2,7 +2,6 @@ export default class Ball {
   constructor(game) {
     
     this.game=game
-    console.log(this.game)
     this.image = document.getElementById("img_pongBall");
 
     this.position = { x: 200, y: 20 };
@@ -30,14 +29,8 @@ export default class Ball {
     //abprallen von Wand
     if (this.position.y > this.gameHeight - this.size) this.speed.y = -5;
     if (this.position.y < 0) this.speed.y = 5;
-    //console.log(this.game.racketL.position)
-    //console.log(this.game.racketL.height)
-    //console.log(this.position.x)
-    //abprallen von den Rackets
+    //abprallen von den Rackets und Punkt ende
     if(this.position.x<this.game.racketL.position.x+this.game.racketL.width){
-      console.log(this.position.y)
-      console.log(this.game.racketL.position.y)
-      console.log(this.game.racketL.position.y+this.game.racketL.height)
       if(this.position.y>=this.game.racketL.position.y&&this.position.y<=this.game.racketL.position.y+this.game.racketL.height){
         this.speed.x*=-1
         console.log("hit L")
@@ -48,9 +41,6 @@ export default class Ball {
       }
     }
     if(this.position.x>this.game.racketR.position.x-this.game.racketR.width){
-      console.log(this.position.y)
-      console.log(this.game.racketR.position.y)
-      console.log(this.game.racketR.position.y+this.game.racketL.height)
       if(this.position.y>=this.game.racketR.position.y&&this.position.y<=this.game.racketR.position.y+this.game.racketR.height){
         this.speed.x*=-1
         console.log("hit R")
