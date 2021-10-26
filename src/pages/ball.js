1732c37aa29ef2bad5aa72559357c1c6c4ac3e9f
Phiedie <p.sautmann@sozialbank.de>
2021-10-26 18:33:02 +0200

    Racket block

--- src/pages/ball.js
+++ src/pages/ball.js
@@ -1,5 +1,8 @@
 export default class Ball {
   constructor(game) {
+    
+    this.game=game
+    console.log(this.game)
     this.image = document.getElementById("img_pongBall");
 
     this.position = { x: 200, y: 20 };
@@ -8,6 +11,7 @@ export default class Ball {
     this.size = 18;
     this.gameHeight = game.gameHeight;
     this.gameWidht = game.gameWidht;
+    this.racketL = game.racketL;
   }
 
   draw(ctx) {
@@ -23,24 +27,41 @@ export default class Ball {
   update(deltaTime) {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
-
+    //abprallen von Wand
     if (this.position.y > this.gameHeight - this.size) this.speed.y = -5;
     if (this.position.y < 0) this.speed.y = 5;
-
-    //Ball score right
-    if (this.position.x > this.gameWidht - this.size) {
-      //spawn towards left
-      this.resetSpawn();
+    //console.log(this.game.racketL.position)
+    //console.log(this.game.racketL.height)
+    //console.log(this.position.x)
+    //abprallen von den Rackets
+    if(this.position.x<this.game.racketL.position.x+this.game.racketL.width){
+      console.log(this.position.y)
+      console.log(this.game.racketL.position.y)
+      console.log(this.game.racketL.position.y+this.game.racketL.height)
+      if(this.position.y>=this.game.racketL.position.y&&this.position.y<=this.game.racketL.position.y+this.game.racketL.height){
+        this.speed.x*=-1
+        console.log("hit L")
+      }else{
+        console.log("miss L")
+        this.resetSpawn();
+        this.speed.x = 5;
+      }
+    }
+    if(this.position.x>this.game.racketR.position.x-this.game.racketR.width){
+      console.log(this.position.y)
+      console.log(this.game.racketR.position.y)
+      console.log(this.game.racketR.position.y+this.game.racketL.height)
+      if(this.position.y>=this.game.racketR.position.y&&this.position.y<=this.game.racketR.position.y+this.game.racketR.height){
+        this.speed.x*=-1
+        console.log("hit R")
+      }else{
+        console.log("miss R")
+        this.resetSpawn();
       this.speed.x = -5;
+      }
+      //if(this.position.y)
     }
-
-    //Ball scored left
-    if (this.position.x < this.size) {
-      //spawn towards right
-      this.resetSpawn();
-
-      this.speed.x = 5;
-    }
+    
   }
 
   resetSpawn() {
