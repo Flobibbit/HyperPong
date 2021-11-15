521577b07cba70492e12b417569fa36c02e59ef0
Phiedie <p.sautmann@sozialbank.de>
2021-11-15 11:21:40 +0100

    first Code Gameover

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -1,18 +1,19 @@
+
 import { GAME_WIDTH, GAME_HEIGHT } from "/src/pages/constant.js";
 import MenuElement from "./MenuElement.js";
+import GameOver from "./GameOver.js";
 
 export default class Score {
   constructor() {
     this.scoreLInt = 0; //number of the score for player ->left side
     this.scoreRInt = 0; //number of the score for player ->right side
-
+    this.gameOverScore = 3;
     this.scoreL = new MenuElement({
       name: "0",
       pxSize: 40,
       locationHeight: GAME_HEIGHT / 8,
       locationWidth: GAME_WIDTH / 4
     });
-    console.log(this.scoreL);
 
     this.scoreR = new MenuElement({
       name: "0",
@@ -26,9 +27,14 @@ export default class Score {
     if (playerSide === "r") {
       this.scoreRInt += 1;
       this.scoreR.name = this.scoreRInt.toString();
+      
     } else {
       this.scoreLInt += 1;
       this.scoreL.name = this.scoreLInt.toString();
+    }
+    if(this.scoreLInt>=this.gameOverScore || this.scoreRInt >= this.gameOverScore){
+      this.GameOverScreen = new GameOver();
+      this.GameOverScreen.GameOverScreen()
     }
   }
   draw(ctx) {
