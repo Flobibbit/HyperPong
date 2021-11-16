ddc786f8adc8172987e3ce55c1a6c850fcf2ee3c
Flobibit <floneuss99gmail.com>
2021-11-16 10:19:24 +0100

    press enter while open a view

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -1,4 +1,3 @@
-
 import { GAME_WIDTH, GAME_HEIGHT } from "/src/pages/constant.js";
 import MenuElement from "./MenuElement.js";
 import GameOver from "./GameOver.js";
@@ -12,14 +11,14 @@ export default class Score {
       name: "0",
       pxSize: 40,
       locationHeight: GAME_HEIGHT / 8,
-      locationWidth: GAME_WIDTH / 4
+      locationWidth: GAME_WIDTH / 4,
     });
 
     this.scoreR = new MenuElement({
       name: "0",
       pxSize: 40,
       locationHeight: GAME_HEIGHT / 8,
-      locationWidth: (GAME_WIDTH / 4) * 3
+      locationWidth: (GAME_WIDTH / 4) * 3,
     });
   }
 
@@ -27,14 +26,17 @@ export default class Score {
     if (playerSide === "r") {
       this.scoreRInt += 1;
       this.scoreR.name = this.scoreRInt.toString();
-      
     } else {
       this.scoreLInt += 1;
       this.scoreL.name = this.scoreLInt.toString();
     }
-    if(this.scoreLInt>=this.gameOverScore || this.scoreRInt >= this.gameOverScore){
+    //view GameOver screen when the players score is as high as the gameOverscore
+    if (
+      this.scoreLInt >= this.gameOverScore ||
+      this.scoreRInt >= this.gameOverScore
+    ) {
       this.GameOverScreen = new GameOver();
-      this.GameOverScreen.GameOverScreen()
+      this.GameOverScreen.GameOverScreen();
     }
   }
   draw(ctx) {
