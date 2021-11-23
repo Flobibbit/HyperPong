7e06c9dcc09ba37066fe22eebd047578bbecbb3e
Phiedie <p.sautmann@sozialbank.de>
2021-11-23 13:06:14 +0100

    game Mods display

--- src/pages/gameMod.js
+++ src/pages/gameMod.js
@@ -3,11 +3,12 @@ import { GAME_WIDTH, GAME_HEIGHT } from "/src/pages/constant.js";
 
 export default class gameMods{
   constructor(){
-    this.modTime=5000.0
+    this.modTime=7000.0
     this.modActive = [false, false, false, false]
+    this.modNames = ["Random Bounce", "Speed Up", "Inverted Controlls"]
     this.modStartTime = [0,0,0,0]
     //timerStart=
-    this.timer=10000.0
+    this.timer=5000.0
     this.timeLeftInt=0
     this.timeLeftStart=0
     this.timeLeft = new MenuElement({
@@ -16,6 +17,7 @@ export default class gameMods{
       locationHeight: GAME_HEIGHT / 8,
       locationWidth: GAME_WIDTH / 2
     });
+    this.modDisplayItems = []
   }
   update(timeStamp){
     if (this.timeLeftStart==0){
@@ -32,22 +34,62 @@ export default class gameMods{
     for (let i in this.modActive){
       if(this.modActive[i]){
         if(timeStamp-this.modStartTime[i]>=this.modTime){
-          this.modActive[i]=false
+          this.modDeactivate(i)
           console.log("Mod" + i +" deaktiviert")
-          console.log(this.modActive)
         }
       }
     }
+
+  }
+  modDeactivate(mod){
+    this.modActive[mod]=false
+    this.manageModDisplay()
+  }
+  manageModDisplay(){
+    var activeMods = []
+    for (let i in this.modActive){
+      if(this.modActive[i]){
+        console.log("Dranhängen: " + i)
+        activeMods.push(
+          new MenuElement({
+            name: this.modNames[i],
+            pxSize: 20,
+            locationHeight: GAME_HEIGHT / 8*7,
+            locationWidth: GAME_WIDTH / 2,
+            color: "#bc42f5"
+          })
+        )
+      }
+    }
+    var amountOfActiveMods = this.amountOfModsActive()
+    for(let i = 0; i<amountOfActiveMods; i++){
+      activeMods[i].locationWidth = GAME_WIDTH / (amountOfActiveMods+1)*(i+1)
+    }
+    this.modDisplayItems = activeMods
+    console.log(this.modDisplayItems)
   }
 
+  amountOfModsActive(){
+    var amountOfActiveMods = 0
+    for(let i of this.modActive){
+      if(i){
+        amountOfActiveMods += 1
+      }
+    }
+    return amountOfActiveMods
+  }
   draw(ctx){
     this.timeLeft.draw(ctx)
+    for(let i of this.modDisplayItems){
+      console.log(i)
+      i.draw(ctx)
+    }
+    
   }
   modActivate(mod, timestamp){
     this.modActive[mod] = true
     this.modStartTime[mod] = timestamp
-    console.log("Mod " + mod + " akiviert")
-    console.log(this.modActive)
+    this.manageModDisplay()
   }
 
   randomBounce() {
